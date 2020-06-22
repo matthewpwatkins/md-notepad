ea742c658fa4bb9ec6dfa5a3571757462ab5cffd
Matthew Watkins <wtkmatt@amazon.com>
2020-06-22 11:44:59 -0500

    Implemented auto-save

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const AUTO_SAVE_INTERVAL = 5000;
 const NEW_FILE_NAME = 'Untitled';
+const TEMP_FILE_PATH = 'temp';
 const FILE_PREFIX = 'file/';
 const MD = new window.remarkable.Remarkable("full");
 
@@ -9,7 +11,10 @@ const app = new Vue({
     data: {
         mounted: false,
         files: [],
-        activeFile: {}
+        activeFile: {},
+        dirty: false,
+        lastSave: 0,
+        saveInProgress: false
     },
     computed: {
         activeFilePreview: function() {
@@ -19,11 +24,12 @@ const app = new Vue({
     mounted() {
         this.initialize();
         this.listenForKeyboardShortcuts();
+        this.startAutoSaveTimer();
         this.mounted = true;
     },
     methods: {
         initialize() {
-            const files = [{ name: NEW_FILE_NAME, active: true, dirty: true }];
+            const files = [{ name: NEW_FILE_NAME, active: true }];
             for (const key in localStorage) {
                 if (key.startsWith(FILE_PREFIX)) {
                     files.push({ name: key.substring(FILE_PREFIX.length) });
@@ -31,37 +37,55 @@ const app = new Vue({
             }
             this.files = files;
             this.activeFile = { index: 0, content: '' };
+            this.openFile(0);
         },
         listenForKeyboardShortcuts() {
             this.keyListener = function(e) {
                 if (e.key === "s" && (e.ctrlKey || e.metaKey)) {
                     e.preventDefault();
                     this.saveActiveFile();
+                } else {
+                    this.dirty = true;
                 }
             };
             document.addEventListener('keydown', this.keyListener.bind(this));
         },
+        startAutoSaveTimer() {
+            this.autoSaveTimer = setInterval(() => {
+                const now = new Date().getTime();
+                const file = this.files[this.activeFile.index];
+                if (this.dirty && !this.saveInProgress && now > this.lastSave + AUTO_SAVE_INTERVAL - 1000) {
+                    this.saveInProgress = true;
+                    const savedAt = new Date().getTime();
+                    this.saveActiveFile();
+                    setTimeout(() => {
+                        this.dirty = false;
+                        this.lastSave = savedAt;
+                        this.saveInProgress = false;
+                    }, 750);
+                }
+            }, AUTO_SAVE_INTERVAL);
+        },
         openFile(fileIndex) {
             const file = this.files[fileIndex];
             this.files[this.activeFile.index].active = false;
+            const filePath = file.name === NEW_FILE_NAME ? TEMP_FILE_PATH : `${FILE_PREFIX}${file.name}`;
             this.activeFile = {
                 index: fileIndex,
-                content: localStorage.getItem(FILE_PREFIX + file.name)
+                content: localStorage.getItem(filePath)
             }
             file.active = true;
         },
         saveActiveFile() {
             const file = this.files[this.activeFile.index];
-            let fileName = file.name;
-            if (fileName === NEW_FILE_NAME) {
-                fileName = prompt('Give your title a file name!');
-            }
-            localStorage.setItem(FILE_PREFIX + fileName, this.activeFile.content);
-            file.name = fileName;
-            file.dirty = false;
+            let filePath = file.name === NEW_FILE_NAME ? TEMP_FILE_PATH : `${FILE_PREFIX}${file.name}`;
+            localStorage.setItem(filePath, this.activeFile.content);
         },
         removeKeyboardShortcuts() {
             document.removeEventListener('keydown', this.keyListener);
+        },
+        stopAutoSaveTimer() {
+            clearInterval(this.autoSaveTimer);
         },
         beforeDestroy() {
             this.removeKeyboardShortcuts();
